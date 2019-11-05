edd61b05c6fe5cd0146d2049002e59119d5b53f1
ignacioTraversi <ignaciotraversi@gmail.com>
2019-11-05 16:55:41 -0300

    Merge

--- nacho.js
+++ nacho.js
@@ -1,8 +1,5 @@
 for (let i = 0; i<Array.length; i++)
 
-<<<<<<< HEAD
 
 console.log("Primera modificacion")
-=======
 //primera modificación compartida
->>>>>>> 62c4c8d3625ad3e6164f12ffa752c7d893527a8c
